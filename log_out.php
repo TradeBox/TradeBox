7675ed2c5fc8064402a8de10ae171887ce352129
Prokostov <kostov@prohost.bg>
2014-09-29 16:44:12 +0300

    archive_add included in login.php , forgot_pass, log_out and reg_admin

--- log_out.php
+++ log_out.php
@@ -1,28 +1,9 @@
 <?php session_start();
 include('../DBconnect/dbconnect.php');
 $userid=$_SESSION['user_id'];
+include('archive_add.php');
 
-function get_client_ip() {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
-	$browserAgent = $_SERVER['HTTP_USER_AGENT'];
-	$ipadd=get_client_ip();
-	mysql_query("INSERT INTO archive (who,action,ip,os) VALUES ('$userid','Потребителят Излезе от системата','$ipadd','$browserAgent')");
+		add_to_archive('Потребителят Излезе от системата.');
 
 session_destroy();
 header("Location: login.php"); 
